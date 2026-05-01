74e20f2e6ea3e227646dc195dd4cfcb3ce6fa520
Calix Sindac <calixgabriel093@gmail.com>
2026-05-01 22:14:04 +0800

    Implemented the retrieval of physical activity logs via extract_activities function

--- src/file_handler.cpp
+++ src/file_handler.cpp
@@ -1,14 +1,29 @@
 #include "headers.cpp"
+#include "structures.cpp"
 
 using namespace std;
 
 bool file_exists(const string &file_path);
+bool extract_activities(const string &file_path, vector<Physical_Activity> &container);
 
 int main()
 {
+    vector<Physical_Activity> temp;
     string s = "myfile.txt";
 
-    file_exists(s);
+    if (file_exists(s))
+    {
+        extract_activities("myfile.txt", temp);
+    }
+
+    for (Physical_Activity act : temp)
+    {
+        cout << "\n------------------------------------\n";
+        cout << "Name: " << act.activity << "\n";
+        cout << "Time Elapsed: " << act.minutes_conducted << "\n";
+        cout << "Calories Burned: " << act.calories_burned << "\n"; 
+    }
+    
 
     return 0;
 }
@@ -33,7 +48,62 @@ bool file_exists(const string &file_path)
     return true;
 }
 
-bool extract_content(const string &file_path)
+bool extract_activities(const string &file_path, vector<Physical_Activity> &container)
 {
+    ifstream file(file_path);
+
+    Physical_Activity temp;
+
+    string text;
+
+    enum Read_State{
+        NONE = -1,
+        NAME = 0,
+        TIME = 1,
+        CAL_BURNED = 2
+    };
+
+    enum Read_State status = NONE;   
+    
+    // Parse text file
+    while(getline(file, text))
+    {
+        if (text == "--ENTRY--")
+        {
+            status = NONE;
+            container.push_back(temp);
+            continue;
+        }else if (text == "-N")
+        {
+            status = NAME;
+            continue;
+        }else if (text == "-T")
+        {
+            status = TIME;
+            continue;
+        }else if (text == "-C")
+        {
+            status = CAL_BURNED;
+            continue;
+        }
+
+        // Create temporary physical activity variable
+        if (status == NAME)
+        {
+            temp.activity = text;
+        }else if (status == TIME)
+        {
+            temp.minutes_conducted = stof(text);
+        }else if (status == CAL_BURNED)
+        {
+            temp.calories_burned = stof(text);
+        }
+
+        status = NONE;
+    }
+
+    
+
+    file.close();
     return true;
 }
