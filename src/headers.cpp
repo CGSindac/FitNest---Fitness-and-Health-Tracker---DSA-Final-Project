0598870e3eb4034457c617990102e71a2333d6c5
Calix Sindac <calixgabriel093@gmail.com>
2026-05-03 21:29:31 +0800

    Added more libraries for qulaity of life, computations, etc.

--- src/headers.cpp
+++ src/headers.cpp
@@ -5,9 +5,19 @@
 
 // Input and Output
 #include <iostream>
+#include <iomanip>
 
 // Data Types & Structures
 #include <map>
 #include <vector>
 #include <string>
+#include <sstream>
+
+// Computations
+#include <cmath> 
+
+// Important / Others
+#include <cstdlib>
+#include <windows.h>
+#pragma once
 
